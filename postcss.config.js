86fb01ce975d74ac5773439fdfa8b5390d6f9703
Josh Pullen <JoshPullen27@gmail.com>
2020-04-05 20:14:16 -0400

    Use purgecss to drastically reduce file size Decrease in page weight here is downright silly

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,16 @@
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: [
+    "./src/**/*.js",
+    "./src/**/*.jsx",
+    "./content/**/*.md",
+    "./content/**/*.mdx"
+  ],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
 module.exports = () => ({
-  plugins: [require("tailwindcss")]
+  plugins: [
+    require("tailwindcss"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+  ]
 });
